dba435ae32cddf763ef89b4776b5fbddc930227f
Ivan Klesov <ivklesov@gmail.com>
2020-10-01 20:10:40 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === [] || arguments.length === 0) {
+      return 0;
+  };
+  let min = 0;
+
+  for (let i = 0; i < array.length; i++) {
+      if ( min > array[i]) {
+          min = array[i];
+      }
+  };
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === [] || arguments.length === 0 ) {
+    return 0;
+  };
+  let max = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if ( max < array[i]) {
+        max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length == 0 || array.length == 0) {
+      return 0;
+    };
+    let total = 0;
+    let avg;
+
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+    };
+
+    avg = total / array.length;
+    return avg;
 }
